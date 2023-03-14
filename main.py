d1c54a4fcf919ace0e06e5f6420773c211b419c6
ydaikai <ydaikai@protonmail.com>
2023-03-14 17:22:56 +0900

    Updated to compatible with gpt-3.5-turbo

--- main.py
+++ main.py
@@ -1,60 +1,51 @@
-import requests
+import discord
+import openai
 import os
 
-# Discord Botのクライアントを作成する
-client = commands.Bot(command_prefix = '!', intents=discord.Intents.all())
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 
-# OpenAIのAPIキーを設定する
-API_KEY = os.environ["OPENAI_TOKEN"]
+from datetime import date
+today = date.today()
+cutoff = "2021-09-01"
 
-# OpenAIのAPIを呼び出すためのエンドポイントを設定する
-API_ENDPOINT = "https://api.openai.com/v1/completions"
+# OpenAI APIのセットアップ
+openai.api_key = os.environ["OPENAI_TOKEN"]
 
-# Discord Botの動作を定義する
-@client.command(name="chat", aliases=["/chat"])  # nameとaliasesを指定する
-async def chat(ctx, *, query: str):
-  
-  # OpenAIのAPIを呼び出し、モデルから返された応答を取得する
-  response = requests.post(
-    API_ENDPOINT,
-    headers={
-      "Content-Type": "application/json",
-      "Authorization": f"Bearer {API_KEY}"
-    },
-    json={
-      "prompt": query,
-      "model": "text-davinci-003",
-      "temperature": 0.6,
-      "max_tokens": 512
-    }
-  )
-  # 応答をDiscord上で表示する
-  await ctx.send(response.json()["choices"][0]["text"])
+chats = [{"role": "system", "content": f"You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible. Knowledge cutoff: {cutoff} Current date: {today}."}]
 
-# Discord Botのトークンを設定して、Botを起動する
-client.run(os.environ["DISCORD_TOKEN"])
+# チャットボットへのリクエスト送信関数
+def send_request(prompt):
+    # リクエストパラメータの設定
+    # APIへのリクエスト送信
+    chats.append({"role": "user", "content": prompt})
+    response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=list(chats)
+    )
+    chats.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
+    # 返答を取得
+    return response['choices'][0]['message']['content']
 
-# @メンションされたときの処理を定義する
+@client.event
+async def on_ready():
+    print(f'{client.user} がログインしました')
+
 @client.event
 async def on_message(message):
-  try:
-    # Botが@メンションされているかどうかを判定する
-    if message.is_mentioned(client.user):
-      # Botが@メンションされたときの処理を実行する
-      response = requests.post(
-        API_ENDPOINT,
-        headers={
-          "Content-Type": "application/json",
-          "Authorization": f"Bearer {API_KEY}"
-        },
-        json={
-          "prompt": message.content,
-          "model": "text-davinci-003",
-          "temperature": 0.6,
-          "max_tokens": 512
-        }
-      )
-      # 応答をDiscord上で表示する
-      await message.channel.send(response.json()["choices"][0]["text"])
-  except Exception as e:
-    print(e)
+  
+    # 自分自身へのメッセージには反応しないようにする
+    if message.author == client.user:
+        return
+    if message.content == "clear":
+        chats = [{"role": "system", "content": f"You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible. Knowledge cutoff: {cutoff} Current date: {today}."}]
+        await message.channel.send("Response history was cleared.")
+        return
+
+    # 発言に対して、OpenAIに問い合わせて返答を生成する
+    response = send_request(message.content)
+    
+    # 返答をDiscordのチャンネルに送信する
+    await message.channel.send(response)
+
+client.run(os.environ["DISCORD_TOKEN"])
